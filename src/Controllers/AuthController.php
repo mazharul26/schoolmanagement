c61b80f35ce8ec97f37a66e22188782ae0cdbac0
abc <abc@example.com>
2018-11-03 21:56:40 +0600

    Important changed

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -5,21 +5,29 @@ namespace Mazharul\Schoolmanagement\Controllers;
 
 use Slim\Http\Request;
 use Slim\Http\Response;
+
 class AuthController extends AppController
 {
+    public function login(Request $request, Response $response)
+    {
+        return $this->getView()->render($response, "signin.twig");
+    }
+
+    public function loginProccessing(Request $request, Response $response){
+
+        return $this->getView()->render($response, "main_dashboard.twig");
+    }
+
     public function signup(Request $request, Response $response)
     {
         return $this->getView()->render($response, "signup.twig");
     }
-    /**
-     * Process login
-     *
-     * @param Request $request
-     * @param Response $response
-     * @return \Psr\Http\Message\ResponseInterface
-     */
-    public function signin(Request $request , Response $response){
-        return $this->getView()->render($response,"signin.twig");
+
+
+    public function signupProccessing(Request $request, Response $response){
+
+        return $this->getView()->render($response, "signin.twig");
+
     }
 
 
