9a9297e8c773dd37069fe11658ff435b5a80d724
abc <abc@example.com>
2018-11-03 21:54:57 +0600

    Important changed

--- app/routes.php
+++ app/routes.php
@@ -1,21 +1,15 @@
 <?php
-/**
- * Common Routes
- */
+
+use Mazharul\Controllers;
 use Mazharul\Schoolmanagement\Controllers\AuthController;
+
 use Slim\Http\Request;
 use Slim\Http\Response;
-$app->get("/", AuthController::class . ":signin");
 
+$app->get("/", AuthController::class . ":login");
+$app->post("/dashboard", AuthController::class . ":loginProccessing");
 $app->get("/signup", AuthController::class . ":signup");
-
-$app->post('/dashboard', function ($request, $response) {
-    return $this->view->render($response, 'main_dashboard.twig');
-});
-$app->get('/signup', function ($request, $response) {
-    return $this->view->render($response, 'signup.twig');
-});
-
+$app->post("/", AuthController::class . ":signupProccessing");
 
 $app->get('/dashboard', function ($request, $response) {
     return $this->view->render($response, 'main_dashboard.twig');
@@ -24,8 +18,8 @@ $app->get('/dashboard', function ($request, $response) {
 $app->get('/form', function ($request, $response) {
     return $this->view->render($response, 'formvalidation.twig');
 });
-$app->get('/contact', function ($request, $response) {
-    return $this->view->render($response, 'contact.twig');
+$app->get('/gellary', function ($request, $response) {
+    return $this->view->render($response, 'gellary.twig');
 });
 $app->get('/button', function ($request, $response) {
     return $this->view->render($response, 'buttongroup.twig');
